b5a9999b71c09244fb913b69f2efdb87bd698a24
weiweitoo <sengweitoo@gmail.com>
2018-08-31 20:10:16 +0800

    pass data from tableinput into piebar

--- src/components/tableinput.vue
+++ src/components/tableinput.vue
@@ -2,52 +2,53 @@
   <div>
     <el-row type="flex" justify="center" class="padding-20">
       <el-col :offset="4" :span="18">
-        <el-form ref="form" :model="newProcess" :inline="true" label-position="left" label-width="100px">
+        <el-form ref="form" :model="NewProcess" :inline="true" label-position="left" label-width="100px">
           <el-form-item label="Name:">
-            <el-input v-model="newProcess.name"></el-input>
+            <el-input v-model="NewProcess.Name"></el-input>
           </el-form-item>
           <el-form-item label="Burst Time:">
-            <el-input v-model="newProcess.burstTime"></el-input>
+            <el-input v-model="NewProcess.BurstTime"></el-input>
           </el-form-item>
           <el-form-item label="Priority:">
-            <el-input v-model="newProcess.priority"></el-input>
+            <el-input v-model="NewProcess.Priority"></el-input>
           </el-form-item>
           <el-form-item label="Arrival Time:">
-            <el-input v-model="newProcess.arrivalTime"></el-input>
+            <el-input v-model="NewProcess.ArrivalTime"></el-input>
           </el-form-item>
-          <el-form-item label="Quantum:" v-if="quantum">
-            <el-input v-model="quantumNum"></el-input>
+          <el-form-item label="Quantum:" v-if="Quantum">
+            <el-input v-model="QuantumNum"></el-input>
           </el-form-item>
         </el-form>
-        <el-button type="primary" @click="createPerson">Add Process</el-button>
+        <el-button type="primary" @click="createProcess">Add Process</el-button>
+        <el-button type="primary" @click="onClickButton">Compute Answer</el-button>
       </el-col>
     </el-row>
     <el-row type="flex" justify="center" class="padding-20">
       <el-col :offset="4" :span="20">
         <el-table
           :header-cell-style="{background : '#89bdd3',color : 'white'}"
-          :data="process"
+          :data="Process"
           border
           stripe
           empty-text="Empty"
           style="width: 100%">
           <el-table-column
-            prop="name"
+            prop="Name"
             label="Process Name"
             width="180">
           </el-table-column>
           <el-table-column
-            prop="burstTime"
+            prop="BurstTime"
             label="Burst Time"
             width="180">
           </el-table-column>
           <el-table-column
-            prop="arrivalTime"
+            prop="ArrivalTime"
             label="Arrival Time"
             width="180">
           </el-table-column>
           <el-table-column
-            prop="priority"
+            prop="Priority"
             label="Priority"
             width="180">
           </el-table-column>
@@ -59,7 +60,7 @@
                <el-button
                  size="mini"
                  type="danger"
-                 @click="deletePerson(scope.$index)">Delete
+                 @click="deletePerocess(scope.$index)">Delete
                </el-button>
            </template>
           </el-table-column>
@@ -76,32 +77,30 @@ export default {
   name: 'tableinput',
   data: function(){
     return {
-      newProcess: {
-        name: '',
-        burstTime: 0,
-        arrivalTime: 0,
-        priority : 0,
+      NewProcess: {
+        Name: '',
+        BurstTime: 0,
+        ArrivalTime: 0,
+        Priority : 0,
       },
-      process: [],
-      quantumNum : 0
+      Process: [],
+      QuantumNum : 0
     }
   },
-  props:['quantum'],
+  props:['Quantum'],
   mounted(){
     console.log(GetAnswer());
   },
   methods: {
-    createPerson: function () {
-      this.process.push(this.newProcess)
-      // 添加完newProcess对象后，重置newProcess对象
-      this.newProcess = {name: '', burstTime: 0, arrivelTime: 0, priority: 0}
+    createProcess: function () {
+      this.Process.push(this.NewProcess)
+      this.NewProcess = {Name: '', BurstTime: 0, ArrivalTime: 0, Priority: 0}
     },
-    deletePerson: function (index) {
-      // 删一个数组元素
-      this.process.splice(index, 1)
+    deletePerocess: function (index) {
+      this.Process.splice(index, 1)
     },
-    onClickButton (event) {
-      this.$emit('clicked', 'someValue')
+    onClickButton: function (event) {
+      this.$emit('clicked', this.Process, this.QuantumNum)
     }
   }
 }
